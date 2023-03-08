04240ffcfe0cc2533161378a9f373470e1c890b2
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-03-08 11:42:29 -0300

    Requirement 57,58,59:profile page components

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const emailUser = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+
+
   return (
     <div>
       <Header />
+      <p data-testid="profile-email">
+        Email:
+        { emailUser.email }
+      </p>
+      <button
+        data-testid="profile-done-btn"
+        onClick={ () => {
+          history.push('/done-recipes');
+        } }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={ () => {
+          history.push('/favorite-recipes');
+        } }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
