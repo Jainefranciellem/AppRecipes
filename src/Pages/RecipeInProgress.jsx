961f9a86a0af5d5c09c86cf912d59bd4a02eed7a
Wallace <wallace_031@hotmail.com>
2023-03-09 10:04:37 -0300

    requirement: 40

--- src/Pages/RecipeInProgress.jsx
+++ src/Pages/RecipeInProgress.jsx
@@ -3,30 +3,36 @@ import { useRouteMatch } from 'react-router-dom';
 import style from './RecipeInProgress.module.css';
 
 function RecipeInProgress() {
-  const [test, setTest] = useState([]);
-  const [done, setDone] = useState({});
+  const [test, setTest] = useState([]); // array used in mocked file
+  const [done, setDone] = useState([]); // marked input array
+  const [goLs, setGoLs] = useState([]); // array retrieved from localstorage
+  const [lockLs, setLockLs] = useState(false);
 
   // const location = useLocation();
   const match = useRouteMatch();
 
   const testDrinks = [
-    { algo: 'Drinks', measure: ['1'] },
-    { algo: 'Drinks', measure: ['1'] },
-    { algo: 'Drinks', measure: ['1'] },
+    'Drinks-1',
+    'Drinks-2',
+    'Drinks-3',
   ];
   const testMeals = [
-    { algo: 'Meals', measure: ['1'] },
-    { algo: 'Meals', measure: ['1'] },
-    { algo: 'Meals', measure: ['1'] },
-    { algo: 'Meals', measure: ['1'] },
-    { algo: 'Meals', measure: ['1'] },
-    { algo: 'Meals', measure: ['1'] },
-    { algo: 'Meals', measure: ['1'] },
-    { algo: 'Meals', measure: ['1'] },
+    'Meals-1',
+    'Meals-2',
+    'Meals-3',
+    'Meals-4',
+    'Meals-5',
+    'Meals-6',
+    'Meals-7',
+    'Meals-8',
   ];
 
+  /**
+   * função resposavel por "setar o localstorage para testar o component"
+   * @param {*} x recebe o ID do item
+   */
   const saveLs = (x) => {
-    localStorage.setItem('inProgressRecipes', JSON
+    localStorage.setItem('inProgress', JSON
       .stringify({ drinks: { [x]: testDrinks }, meals: { [x]: testMeals } }));
   };
 
@@ -40,13 +46,58 @@ function RecipeInProgress() {
     saveLs(recipeKey); // apenas para testes <-----------
 
     const lsSaved = JSON.parse(localStorage
-      .getItem('inProgressRecipes'))[mealsOrDrinks][recipeKey] || [];
+      .getItem('inProgress'))[mealsOrDrinks][recipeKey];
     setTest(lsSaved);
   };
 
+  /**
+   * retrieves information from localstorage and saves it in the "goLs" state to update already tagged entries
+   */
+  const checkedInputRecovery = () => {
+    const mealsOrDrinks = match.url.split('/')[1]; // captures whether it is the meals or drinks route to be used as a key
+    const recipeKey = match.params.id; // captures the id of the recipe to be used as a key
+
+    const ls = JSON.parse(localStorage.getItem('inProgressRecipes')) ?? {
+      meals: {},
+      drinks: {},
+    };
+
+    setGoLs(ls[mealsOrDrinks][recipeKey]);
+  };
+
+  /**
+   * function responsible for updating the localstorage with the marked inputs
+   */
+  const checkedList = () => {
+    const mealsOrDrinks = match.url.split('/')[1]; // captures whether it is the meals or drinks route to be used as a key
+    const recipeKey = match.params.id; // captures the id of the recipe to be used as a key
+
+    const algo2 = JSON.parse(localStorage.getItem('inProgressRecipes')) ?? {
+      meals: {},
+      drinks: {},
+    };
+
+    localStorage.setItem('inProgressRecipes', JSON
+      .stringify({
+        ...algo2,
+        [mealsOrDrinks]: {
+          ...algo2[mealsOrDrinks],
+          [recipeKey]: done,
+        },
+      }));
+  };
+
+  /**
+   * useEffect responsible for calling the function responsible for recovering the localstorage state
+   */
   useEffect(() => {
     lsRecovery();
   }, []);
+
+  useEffect(() => {
+    if (lockLs) { checkedList(); }
+    checkedInputRecovery();
+  }, [done]);
 
   return (
     <div>
@@ -74,19 +125,27 @@ function RecipeInProgress() {
       <div className={ style.list }>
         {test?.map((ingredientRecipe, i) => (
           <label
-            key={ `${ingredientRecipe.algo}-${i}` }
+            key={ `${ingredientRecipe}-${i}` }
             data-testid={ `${i}-ingredient-step` }
-            className={ `${done[i] && style['list-of-ingredients']}` }
+            className={ `${goLs
+              ?.includes(ingredientRecipe) && style['list-of-ingredients']}` }
           >
             <input
               type="checkbox"
               name=""
-              id="ingredient"
-              onChange={ ({ target }) => {
-                setDone((prev) => ({ ...prev, [i]: target.checked }));
+              checked={ goLs?.includes(ingredientRecipe) }
+              // id="ingredient"
+              onChange={ () => {
+                setDone((prev) => {
+                  if (prev.includes(ingredientRecipe)) {
+                    return prev.filter((element) => element !== ingredientRecipe);
+                  }
+                  return [...prev, ingredientRecipe];
+                });
+                setLockLs(true);
               } }
             />
-            { `${ingredientRecipe.algo}: ${ingredientRecipe.measure[0]}` }
+            { `${ingredientRecipe}` }
           </label>
         ))}
 
